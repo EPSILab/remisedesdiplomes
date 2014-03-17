b5e82cba46a4ef723cb51bf8224a01a156e8e77f
Paul Boré <bore-paul@voila.fr>
2014-03-17 17:11:49 +0100

    Project creation, add Guest and GuestManager classes

---
+++ EpsiLab.RemiseDesDiplomes/EpsiLab.RemiseDesDiplomes/Guest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EpsiLab.RemiseDesDiplomes
+{
+    public class Guest
+    {
+        public Guest(string firstName, string lastName)
+        {
+            /// TODO
+        }
+
+        private string _firstName;
+        public string FirstName 
+        {
+            get { return _firstName;}
+            set 
+            { 
+                if (!String.IsNullOrEmpty(value))
+                    _firstName = value;
+            }
+        }
+
+        
+        public string LastName { get; set; }
+
+        public bool IsHere { get; set; }
+        public DateTime DateVenue { get; set; }
+    }
+}
